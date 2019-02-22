26517d00c773942af7f943be40d28aa5eb3399df
Audrey75019 <47895368+Audrey75019@users.noreply.github.com>
2019-02-22 16:42:22 +0100

    Version initiale

---
+++ 03_site/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
+	 <title>03 Site</title>
+	<link rel="shortcut icon" href="http://php.net/favicon.ico">
+	<link rel="stylesheet" type="text/css" href="site.css">
+ </head>
+ <body>
+ <h1>03 SITE</h1>
+	 <title>04 Site</title>
+=======
+
+	 <title>05 Site</title>
+>>>>>>> bootstrap
+	<link rel="shortcut icon" href="http://php.net/favicon.ico">
+	<link rel="stylesheet" type="bootstrap" href="site.css">
+ </head>
+ <body>
+ <h1>05 SITE</h1>
+ </body>
+ </html>
